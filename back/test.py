73e5c4856429b83fab56af1ebb661f1c2906de97
marcottemickyncrea <120030827+marcottemickyncrea@users.noreply.github.com>
2023-11-10 11:10:03 +0100

    test 15

---
+++ back/test.py
@@ -0,0 +1,19 @@
+import unittest
+from fastapi.testclient import TestClient
+from app import app
+
+class TestAPI(unittest.TestCase):
+    client = TestClient(app)
+    data = "Le chlorure de sodium se retrouve dans toutes les mers, ainsi que dans les cuisines de toutes les familles, il est plus communément appelé sel. Le dioxyde de carbone est un déchet de la respiration animale."
+    
+    def test_reponse(self):
+        """
+        Vérifie que la reponse est correcte
+        """
+        reponse=self.client.post("/predict",
+                     json=self.data)
+        self.assertEqual(reponse.status_code,200)
+        self.assertEqual(dict,type(reponse.json()))
+
+if __name__ == '__main__':
+    unittest.main()
